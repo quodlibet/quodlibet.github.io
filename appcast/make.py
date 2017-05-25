e4bd117d6546b4ed268cf110920fa6f508b4ad62
Christoph Reiter <reiter.christoph@gmail.com>
2017-05-25 17:16:22 +0200

    Update appcast

--- appcast/make.py
+++ appcast/make.py
@@ -11,11 +11,10 @@ import urllib2
 import feedparser
 from multiprocessing.pool import ThreadPool
 
-
-BITBUCKET = "https://bitbucket.org/lazka/quodlibet/downloads/"
-TARBALLS = "https://bitbucket.org/lazka/quodlibet-files/raw/default/releases/"
+GITHUB = "https://github.com/quodlibet/quodlibet/releases/download/release-%(version)s/"
 
 RELEASES = [
+    "3.9.0 (2017-05-24)",
     "3.8.1 (2017-01-23)",
     "3.8.0 (2016-12-29)",
     "3.7.1 (2016-09-25)",
@@ -52,85 +51,90 @@ BUILDS = {
         "title": "Quod Libet (OS X)",
         "os": "",
         "releases": [
-            ("3.8.1", "0", BITBUCKET + "QuodLibet-%s.dmg"),
-            ("3.8.0", "0", BITBUCKET + "QuodLibet-%s.dmg"),
-            ("3.7.1", "0", BITBUCKET + "QuodLibet-%s.dmg"),
-            ("3.7.0", "0", BITBUCKET + "QuodLibet-%s.dmg"),
-            ("3.6.1", "0", BITBUCKET + "QuodLibet-%s.zip"),
-            ("3.5.2", "1", BITBUCKET + "QuodLibet-%s-v2.zip"),
-            ("3.5.2", "0", BITBUCKET + "QuodLibet-%s.zip"),
-            ("3.5.1", "0", BITBUCKET + "QuodLibet-%s.zip"),
-            ("3.5.0", "1", BITBUCKET + "QuodLibet-%s-v2.zip"),
-            ("3.5.0", "0", BITBUCKET + "QuodLibet-%s.zip"),
-            ("3.4.1", "1", BITBUCKET + "QuodLibet-%s.zip"),
-            ("3.4.1", "0", BITBUCKET + "QuodLibet-%s.zip"),
+            ("3.9.0", "0", GITHUB + "QuodLibet-%(version)s.dmg"),
+            ("3.8.1", "0", GITHUB + "QuodLibet-%(version)s.dmg"),
+            ("3.8.0", "0", GITHUB + "QuodLibet-%(version)s.dmg"),
+            ("3.7.1", "0", GITHUB + "QuodLibet-%(version)s.dmg"),
+            ("3.7.0", "0", GITHUB + "QuodLibet-%(version)s.dmg"),
+            ("3.6.1", "0", GITHUB + "QuodLibet-%(version)s.zip"),
+            ("3.5.2", "1", GITHUB + "QuodLibet-%(version)s-v2.zip"),
+            ("3.5.2", "0", GITHUB + "QuodLibet-%(version)s.zip"),
+            ("3.5.1", "0", GITHUB + "QuodLibet-%(version)s.zip"),
+            ("3.5.0", "1", GITHUB + "QuodLibet-%(version)s-v2.zip"),
+            ("3.5.0", "0", GITHUB + "QuodLibet-%(version)s.zip"),
+            ("3.4.1", "1", GITHUB + "QuodLibet-%(version)s.zip"),
+            ("3.4.1", "0", GITHUB + "QuodLibet-%(version)s.zip"),
         ],
     },
     "osx-exfalso": {
         "title": "Ex Falso (OS X)",
         "os": "",
         "releases": [
-            ("3.8.1", "0", BITBUCKET + "ExFalso-%s.dmg"),
-            ("3.8.0", "0", BITBUCKET + "ExFalso-%s.dmg"),
-            ("3.7.1", "0", BITBUCKET + "ExFalso-%s.dmg"),
-            ("3.7.0", "0", BITBUCKET + "ExFalso-%s.dmg"),
-            ("3.6.1", "0", BITBUCKET + "ExFalso-%s.zip"),
-            ("3.5.2", "1", BITBUCKET + "ExFalso-%s-v2.zip"),
-            ("3.5.2", "0", BITBUCKET + "ExFalso-%s.zip"),
-            ("3.5.1", "0", BITBUCKET + "ExFalso-%s.zip"),
-            ("3.5.0", "1", BITBUCKET + "ExFalso-%s-v2.zip"),
-            ("3.5.0", "0", BITBUCKET + "ExFalso-%s.zip"),
-            ("3.4.1", "1", BITBUCKET + "ExFalso-%s.zip"),
-            ("3.4.1", "0", BITBUCKET + "ExFalso-%s.zip"),
+            ("3.9.0", "0", GITHUB + "ExFalso-%(version)s.dmg"),
+            ("3.8.1", "0", GITHUB + "ExFalso-%(version)s.dmg"),
+            ("3.8.0", "0", GITHUB + "ExFalso-%(version)s.dmg"),
+            ("3.7.1", "0", GITHUB + "ExFalso-%(version)s.dmg"),
+            ("3.7.0", "0", GITHUB + "ExFalso-%(version)s.dmg"),
+            ("3.6.1", "0", GITHUB + "ExFalso-%(version)s.zip"),
+            ("3.5.2", "1", GITHUB + "ExFalso-%(version)s-v2.zip"),
+            ("3.5.2", "0", GITHUB + "ExFalso-%(version)s.zip"),
+            ("3.5.1", "0", GITHUB + "ExFalso-%(version)s.zip"),
+            ("3.5.0", "1", GITHUB + "ExFalso-%(version)s-v2.zip"),
+            ("3.5.0", "0", GITHUB + "ExFalso-%(version)s.zip"),
+            ("3.4.1", "1", GITHUB + "ExFalso-%(version)s.zip"),
+            ("3.4.1", "0", GITHUB + "ExFalso-%(version)s.zip"),
         ],
     },
     "windows": {
         "title": "Quod Libet / Ex Falso (Windows)",
         "os": "windows",
         "releases": [
-            ("3.8.1", "0", BITBUCKET + "quodlibet-%s-installer.exe"),
-            ("3.8.0", "0", BITBUCKET + "quodlibet-%s-installer.exe"),
-            ("3.7.1", "0", BITBUCKET + "quodlibet-%s-installer.exe"),
-            ("3.7.0", "0", BITBUCKET + "quodlibet-%s-installer.exe"),
-            ("3.6.1", "0", BITBUCKET + "quodlibet-%s-installer.exe"),
-            ("3.6.0", "0", BITBUCKET + "quodlibet-%s-installer.exe"),
-            ("3.5.2", "0", BITBUCKET + "quodlibet-%s-installer.exe"),
-            ("3.5.1", "0", BITBUCKET + "quodlibet-%s-installer.exe"),
-            ("3.5.0", "0", BITBUCKET + "quodlibet-%s-installer.exe"),
-            ("3.4.1", "0", BITBUCKET + "quodlibet-%s-installer.exe"),
+            ("3.9.0", "0", GITHUB + "quodlibet-%(version)s-installer.exe"),
+            ("3.8.1", "0", GITHUB + "quodlibet-%(version)s-installer.exe"),
+            ("3.8.0", "0", GITHUB + "quodlibet-%(version)s-installer.exe"),
+            ("3.7.1", "0", GITHUB + "quodlibet-%(version)s-installer.exe"),
+            ("3.7.0", "0", GITHUB + "quodlibet-%(version)s-installer.exe"),
+            ("3.6.1", "0", GITHUB + "quodlibet-%(version)s-installer.exe"),
+            ("3.6.0", "0", GITHUB + "quodlibet-%(version)s-installer.exe"),
+            ("3.5.2", "0", GITHUB + "quodlibet-%(version)s-installer.exe"),
+            ("3.5.1", "0", GITHUB + "quodlibet-%(version)s-installer.exe"),
+            ("3.5.0", "0", GITHUB + "quodlibet-%(version)s-installer.exe"),
+            ("3.4.1", "0", GITHUB + "quodlibet-%(version)s-installer.exe"),
         ],
     },
     "windows-portable": {
         "title": "Quod Libet / Ex Falso (Windows Portable)",
         "os": "windows",
         "releases": [
-            ("3.8.1", "0", BITBUCKET + "quodlibet-%s-portable.exe"),
-            ("3.8.0", "0", BITBUCKET + "quodlibet-%s-portable.exe"),
-            ("3.7.1", "0", BITBUCKET + "quodlibet-%s-portable.exe"),
-            ("3.7.0", "0", BITBUCKET + "quodlibet-%s-portable.exe"),
-            ("3.6.1", "0", BITBUCKET + "quodlibet-%s-portable.exe"),
-            ("3.6.0", "0", BITBUCKET + "quodlibet-%s-portable.exe"),
-            ("3.5.2", "0", BITBUCKET + "quodlibet-%s-portable.exe"),
-            ("3.5.1", "0", BITBUCKET + "quodlibet-%s-portable.exe"),
-            ("3.5.0", "0", BITBUCKET + "quodlibet-%s-portable.exe"),
-            ("3.4.1", "0", BITBUCKET + "quodlibet-%s-portable.exe"),
+            ("3.9.0", "0", GITHUB + "quodlibet-%(version)s-portable.exe"),
+            ("3.8.1", "0", GITHUB + "quodlibet-%(version)s-portable.exe"),
+            ("3.8.0", "0", GITHUB + "quodlibet-%(version)s-portable.exe"),
+            ("3.7.1", "0", GITHUB + "quodlibet-%(version)s-portable.exe"),
+            ("3.7.0", "0", GITHUB + "quodlibet-%(version)s-portable.exe"),
+            ("3.6.1", "0", GITHUB + "quodlibet-%(version)s-portable.exe"),
+            ("3.6.0", "0", GITHUB + "quodlibet-%(version)s-portable.exe"),
+            ("3.5.2", "0", GITHUB + "quodlibet-%(version)s-portable.exe"),
+            ("3.5.1", "0", GITHUB + "quodlibet-%(version)s-portable.exe"),
+            ("3.5.0", "0", GITHUB + "quodlibet-%(version)s-portable.exe"),
+            ("3.4.1", "0", GITHUB + "quodlibet-%(version)s-portable.exe"),
         ],
     },
     "default": {
         "title": "Quod Libet / Ex Falso",
         "os": "linux",
         "releases": [
-            ("3.8.1", "0", TARBALLS + "quodlibet-%s.tar.gz"),
-            ("3.8.0", "0", TARBALLS + "quodlibet-%s.tar.gz"),
-            ("3.7.1", "0", TARBALLS + "quodlibet-%s.tar.gz"),
-            ("3.7.0", "0", TARBALLS + "quodlibet-%s.tar.gz"),
-            ("3.6.2", "0", TARBALLS + "quodlibet-%s.tar.gz"),
-            ("3.6.1", "0", TARBALLS + "quodlibet-%s.tar.gz"),
-            ("3.6.0", "0", TARBALLS + "quodlibet-%s.tar.gz"),
-            ("3.5.2", "0", TARBALLS + "quodlibet-%s.tar.gz"),
-            ("3.5.1", "0", TARBALLS + "quodlibet-%s.tar.gz"),
-            ("3.5.0", "0", TARBALLS + "quodlibet-%s.tar.gz"),
-            ("3.4.1", "0", TARBALLS + "quodlibet-%s.tar.gz"),
+            ("3.9.0", "0", GITHUB + "quodlibet-%(version)s.tar.gz"),
+            ("3.8.1", "0", GITHUB + "quodlibet-%(version)s.tar.gz"),
+            ("3.8.0", "0", GITHUB + "quodlibet-%(version)s.tar.gz"),
+            ("3.7.1", "0", GITHUB + "quodlibet-%(version)s.tar.gz"),
+            ("3.7.0", "0", GITHUB + "quodlibet-%(version)s.tar.gz"),
+            ("3.6.2", "0", GITHUB + "quodlibet-%(version)s.tar.gz"),
+            ("3.6.1", "0", GITHUB + "quodlibet-%(version)s.tar.gz"),
+            ("3.6.0", "0", GITHUB + "quodlibet-%(version)s.tar.gz"),
+            ("3.5.2", "0", GITHUB + "quodlibet-%(version)s.tar.gz"),
+            ("3.5.1", "0", GITHUB + "quodlibet-%(version)s.tar.gz"),
+            ("3.5.0", "0", GITHUB + "quodlibet-%(version)s.tar.gz"),
+            ("3.4.1", "0", GITHUB + "quodlibet-%(version)s.tar.gz"),
         ],
     },
 }
@@ -163,9 +167,13 @@ length="%(length)s" type="application/octet-stream" />
 
 
 def get_size(url):
-    r = urllib2.urlopen(url)
-    length = r.info().get("Content-Length", "0")
-    r.close()
+    try:
+        r = urllib2.urlopen(url)
+        length = r.info().get("Content-Length", "0")
+        r.close()
+    except:
+        print url
+        raise
     return url, length
 
 
@@ -179,7 +187,7 @@ def main():
     url_lengths = {}
     for t in BUILDS.values():
         for r in t["releases"]:
-            url_lengths[r[-1] % r[0]] = ""
+            url_lengths[r[-1] % {"version": r[0]}] = ""
 
     pool = ThreadPool(20)
     for url, length in pool.imap_unordered(get_size, url_lengths):
@@ -193,7 +201,7 @@ def main():
         os_ = type_["os"]
         for version, build, url in type_["releases"]:
             print type_id, version
-            url = url % version
+            url = url % {"version": version}
             if url in url_lengths:
                 length = url_lengths[url]
             else:
